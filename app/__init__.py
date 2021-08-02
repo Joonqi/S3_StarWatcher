65d736e27849065181f2495e106b0d85cd41bbef
joonki <83646259+Joonqi@users.noreply.github.com>
2021-08-02 20:15:51 +0900

    deployment

--- app/__init__.py
+++ app/__init__.py
@@ -9,11 +9,9 @@ db = SQLAlchemy()
 migrate = Migrate()
 
 def create_app():
-    
+
     app = Flask(__name__)
-    # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://fmjxhhfddaofdq:51daa2f796d1167916c3348865ed4175d80087fcac50255f33907f5fd7da6ea9@ec2-44-194-145-230.compute-1.amazonaws.com:5432/d6lbt10a3usids'
-    app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:qwerqwer@localhost:5432/watchapp"
-    # app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db.sqlite3"
+   
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     env_config = os.getenv("APP_SETTINGS", 'config.DevelopmentConfig')
     app.config.from_object(env_config)
