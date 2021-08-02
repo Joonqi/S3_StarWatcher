900cbf6824385e2f8bed0732267afe5677e236e3
joonki <83646259+Joonqi@users.noreply.github.com>
2021-08-02 20:00:18 +0900

    deployment

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn 'twit_app:create_app()'
+web: gunicorn 'app:create_app()'
--- config.py
+++ config.py
@@ -6,12 +6,13 @@ class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
-    SECRET_KEY = 'qwerqwer'
+    
     # SQLALCHEMY_DATABASE_URL = os.environ['DATABASE_URL']
 
 class ProductionConfig(Config):
     DEBUG = False
-
+    SECRET_KEY = '51daa2f796d1167916c3348865ed4175d80087fcac50255f33907f5fd7da6ea9'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://fmjxhhfddaofdq:51daa2f796d1167916c3348865ed4175d80087fcac50255f33907f5fd7da6ea9@ec2-44-194-145-230.compute-1.amazonaws.com:5432/d6lbt10a3usids'
 
 class StagingConfig(Config):
     DEVELOPMENT = True
@@ -21,7 +22,8 @@ class StagingConfig(Config):
 class DevelopmentConfig(Config):
     DEVELOPMENT = True
     DEBUG = True
-
+    SECRET_KEY = 'qwerqwer'
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:qwerqwer@localhost:5432/watchapp"
 
 class TestingConfig(Config):
     TESTING = True
