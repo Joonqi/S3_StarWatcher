a74c21525998fc1b92268a18aabe0ec5527a0c1e
joonki <83646259+Joonqi@users.noreply.github.com>
2021-08-03 11:14:05 +0900

    correct typos

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@
 
 import os
 from rq import Queue
-from flask import Flask, render_template
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
@@ -11,8 +11,8 @@ migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://fmjxhhfddaofdq:51daa2f796d1167916c3348865ed4175d80087fcac50255f33907f5fd7da6ea9@ec2-44-194-145-230.compute-1.amazonaws.com:5432/d6lbt10a3usids'
-    # app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:qwerqwer@localhost:5432/watchapp"
+    # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://fmjxhhfddaofdq:51daa2f796d1167916c3348865ed4175d80087fcac50255f33907f5fd7da6ea9@ec2-44-194-145-230.compute-1.amazonaws.com:5432/d6lbt10a3usids'
+    app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:qwerqwer@localhost:5432/watchapp"
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     env_config = os.getenv("APP_SETTINGS", 'config.StagingConfig')
     app.config.from_object(env_config)
